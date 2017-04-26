25bee356c41885a7b23d8b48b8a3e1b6bb48772a
Kevin Chan <kschan@mit.edu>
2017-04-26 17:03:22 -0400

    update to robotstate, basic RRT tree

--- src/PathPlanning/RobotState.py
+++ src/PathPlanning/RobotState.py
@@ -6,6 +6,11 @@ class RobotState:
         self.theta = theta
         # TODO is this vector oriented right???
         self.orientation = np.array([np.sin(theta), np.cos(theta)])
+
+    def __init__(self, position, theta):
+        self.position = position
+        self.theta = theta
+        self.oreitnation = np.array([np.sin(theta), np.cos(theta)])
     
     def getPosition(self):
         return self.position
