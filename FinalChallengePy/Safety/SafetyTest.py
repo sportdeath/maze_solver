823eaf60db2826314f3507b2aa8075c542becaba
Katy Muhlrad <kmuhlrad@mit.edu>
2017-05-08 16:30:42 -0400

    working safety node in simulation

--- FinalChallengePy/Safety/SafetyTest.py
+++ FinalChallengePy/Safety/SafetyTest.py
@@ -7,23 +7,28 @@ and the joystick overrides safety controller anyways
 '''
 
 import rospy
+
 from ackermann_msgs.msg import AckermannDriveStamped
 
 class SafetyTest:
-	def __init__(self):
+    def __init__(self):
 
-		self.commandPub = rospy.Publisher(
-			"/vesc/high_level/ackermann_cmd_mux/input/nav_0",
-			AckermannDriveStamped, queue_size = 1)
+        self.commandPub = rospy.Publisher(
+            "/vesc/high_level/ackermann_cmd_mux/input/nav_0",
+            AckermannDriveStamped, queue_size = 1)
 
-		self.drive()
+        rospy.sleep(1)
 
-	def drive(self):
-		msg = AckermannDriveStamped()
-        msg.drive.speed = 2
-        msg.drive.steering_angle = 0
+        while self.commandPub.get_num_connections() > 0:
+            self.drive()
+            rospy.sleep(0.2)
+
+    def drive(self):
+        message = AckermannDriveStamped()
+        message.drive.speed = 2
+        message.drive.steering_angle = 0
         
-        self.commandPub.publish(msg)
+        self.commandPub.publish(message)
 
 if __name__=="__main__":
     rospy.init_node("SafetyTest")
