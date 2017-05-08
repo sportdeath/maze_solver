2cad9823f497ccfbd6b86ceaa4bf35947ec7a04b
The Stig <speedracers@mit.edu>
2017-05-08 15:54:51 -0400

    Added proper transformations

--- FinalChallengePy/Vision/CameraCalibration/GenerateTransformationMatrix.py
+++ FinalChallengePy/Vision/CameraCalibration/GenerateTransformationMatrix.py
@@ -11,7 +11,7 @@ from FinalChallengePy.Vision.Constants import *
 class TransformationGenerator:
     def __init__(self):
         # Real world values
-        self.sources = [np.array([0,0,0]), np.array([1,1,0])]
+        self.sources = [np.array([0.,0.,1.]), np.array([1.,1.,1.])]
 
         # Get pixel values
         self.pixelCoords = []
@@ -26,26 +26,40 @@ class TransformationGenerator:
         # Add the pixel value
         x = msg.x
         y = msg.y
-        point = np.array([x, y])
+        point = np.array([x, y, 1.])
         print("Clicked point:", point)
 
+        self.pixelCoords.append(point)
+
         # Generate the transformation
-        if len(pixelCoords) >= 2:
+        if len(self.pixelCoords) >= 2:
             self.generateTransformation()
 
     def generateTransformation(self):
+        cameraMatrixInv = np.loadtxt(CAMERA_MATRIX_INV_FILE)
+
+        print("Generating transformations")
         destinations = [0]*2
         for i in xrange(2):
-            destinations[i] = dot(cameraMatrixInv, self.pixelCoords[i])
+            destinations[i] = np.dot(cameraMatrixInv, self.pixelCoords[i])
 
         (scalingFactor, rotationMatrix, translationVector) = \
                 getTransformation(self.sources, destinations)
 
         # Write them to file
-        np.savetxt(SCALING_FACTOR_FILE, scalingFactor)
+        np.savetxt(SCALING_FACTOR_FILE, np.array([scalingFactor]))
         np.savetxt(TRANSLATION_VECTOR_FILE, translationVector)
         np.savetxt(ROTATION_MATRIX_FILE, rotationMatrix)
         np.savetxt(ROTATION_MATRIX_INV_FILE, np.linalg.inv(rotationMatrix))
+        print("Written to file")
+
+        from FinalChallengePy.Vision.CoordinateTransformations import CoordinateTransformations
+
+        for i in xrange(2):
+            world = CoordinateTransformations.pixelsToWorld(self.pixelCoords[i])
+            print("World: ", world, ", expected: ", self.sources[i])
+            pixels = CoordinateTransformations.worldToPixels(self.sources[i])
+            print("Pixels: ", pixels, ", expected: ", self.pixelCoords[i])
 
 if __name__=="__main__":
     rospy.init_node("GenerateTransformationMatrix")
