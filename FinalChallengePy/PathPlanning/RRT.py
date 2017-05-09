Sampling now kind of working. There is a deep bug in angle calculation. Time to fix!

FinalChallengePy/PathPlanning/RRT.py
@@ -3,6 +3,7 @@ import numpy as np
 from rtree import index
 
 from FinalChallengePy.PathPlanning.Steer import Steer
+from FinalChallengePy.PathPlanning.Sampling import Sampling
 from FinalChallengePy.PathPlanning.RobotState import RobotState
 from FinalChallengePy.PathPlanning.MapUtils import MapUtils
 from FinalChallengePy.PathPlanning.Constants import *
@@ -21,8 +22,8 @@ class RRT:
             numOptimizations = DEFAULT_NUM_OPTIMIZATIONS, 
             verbose = False,
             gaussianProbability = 0.,
-            positionStdDev = 4.
-            angleStdDev = 0.8
+            positionStdDev = 0.5,
+            angleStdDev = 0.5
             ):
         self.mapMsg = mapMsg
         if rangeMethod is None:
@@ -52,7 +53,8 @@ class RRT:
         y = state.getPosition()[1]
         return [tree[i] for i in nnTree.nearest((x, y, x, y), k)]
 
-    def computePath(self, init, goal, backwards=False, fakeWall = None, multipleGoals = False):
+    def computePath(self, init, goal, backwards=False, fakeWall = None, multipleGoals = False, sampleStates = []):
+        print("yo?")
         tree = [RRTNode(init, 0, None)]
         nnTree = index.Index()
         RRT.nnTreeInsertLastElement(nnTree, tree)
@@ -82,7 +84,14 @@ class RRT:
         while self.maxIterations < 0 or i < self.maxIterations:
             if self.verbose:
                 print(i)
-            newState, goal, goalIndex = self.updateTree(tree,nnTree,newState,init,goalStates,backwards, fakeWall = fakeWall)
+            newState, goal, goalIndex = self.updateTree(
+                    tree,
+                    nnTree,
+                    newState,
+                    goalStates,
+                    backwards, 
+                    fakeWall = fakeWall, 
+                    sampleStates = sampleStates)
             if goal:
                 if tree[-1].cost < bestCost:
                     bestGoalIndex = goalIndex
@@ -96,13 +105,13 @@ class RRT:
             if self.verbose:
                 print("Found goal, optimizing")
             self.path = RRT.treeToPath(tree, bestGoal)
-            self.optimize([init]+goalStates, backwards, fakeWall)
+            self.optimize(sampleStates, backwards, fakeWall)
             return bestGoalIndex, tree
 
         self.path = [(init.getPosition(), False)]
         return -1, tree
 
-    def updateTree(self, tree, nnTree, newState, init, goalStates, backwards, fakeWall = None):
+    def updateTree(self, tree, nnTree, newState, goalStates, backwards, fakeWall = None,  sampleStates = []):
         # Check whether last state we added 
         # has steerable path to goal
         if newState:
@@ -120,7 +129,7 @@ class RRT:
                     return False, True, goalIndex
             newState = False
 
-        randomState = self.getRandomState([init]+goals, backwards)
+        randomState = self.getRandomState(sampleStates, backwards)
 
         minNode = None
         minCost = np.inf
@@ -145,7 +154,7 @@ class RRT:
     def getRandomState(self, states, backwards):
         choice = np.random.rand()
         if choice < self.gaussianProbability:
-            # Choose state from either init or goal
+            # Choose state
             index = np.random.randint(len(states))
             state = states[index]
             return Sampling.getGaussianState(state, self.positionStdDev, self.angleStdDev)

FinalChallengePy/PathPlanning/Sampling.py
@@ -5,8 +5,8 @@ from FinalChallengePy.PathPlanning.RobotState import RobotState
 class Sampling:
     @staticmethod
     def getGaussianState(state, positionStdDev, angleStdDev, backwards=False):
-        x, y = np.random.normal(loc=state.getPosition, scale=self.positionStdDev)
-        theta = np.random.normal(loc=state.getTheta(), scale=self.angleStdDev))
+        x, y = np.random.normal(loc=state.getPosition(), scale=positionStdDev)
+        theta = np.random.normal(loc=state.getTheta(), scale=angleStdDev)
         
         if backwards:
             backwards = bool(np.random.randomint(2))

FinalChallengePy/PathPlanning/Test_Sampling.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseArray
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import PointStamped
 
@@ -11,12 +11,13 @@ from FinalChallengePy.PathPlanning.MapUtils import MapUtils
 from FinalChallengePy.PathPlanning.VisualizeLine import VisualizeLine
 from FinalChallengePy.PathPlanning.FakeWall import FakeWall
 from FinalChallengePy.PathPlanning.Steer import Steer 
+from FinalChallengePy.PathPlanning.Sampling import Sampling
 
 from FinalChallengePy.PathPlanning.RobotState import RobotState
 
-class Test_FakeWall(VisualizeLine):
+class Test_Sampling(VisualizeLine):
     def __init__(self):
-        VisualizeLine.__init__(self,"Test_FakeWall", numPublishers = 3)
+        VisualizeLine.__init__(self,"Test_Sampling", numPublishers = 3)
 
         self.mapMsg = MapUtils.getMap()
         self.rangeLib = MapUtils.getRangeLib(self.mapMsg)
@@ -27,19 +28,10 @@ class Test_FakeWall(VisualizeLine):
 
         self.direction = 1.
 
-        self.circleCenter = np.array([-11.9,0.])
-        self.radius = 1.
-
         self.clickSub = rospy.Subscriber(
                 "/move_base_simple/goal", 
                 PoseStamped, 
                 self.clickedPose, 
-                queue_size=1)
-        
-        self.clickSub = rospy.Subscriber(
-                "/initialpose", 
-                PoseWithCovarianceStamped, 
-                self.clickedCircle, 
                 queue_size=1)
 
         self.clickSub = rospy.Subscriber(
@@ -48,18 +40,14 @@ class Test_FakeWall(VisualizeLine):
                 self.clickedPoint, 
                 queue_size=1)
 
+        self.samplePub  = rospy.Publisher(
+                "/samples", 
+                PoseArray, 
+                queue_size = 1)
+
         for i in xrange(3):
             rospy.sleep(0.1)
             self.makeFakeWall()
-
-    def clickedCircle(self, msg):
-        # The received pose
-        x = msg.pose.pose.position.x
-        y = msg.pose.pose.position.y
-
-        self.circleCenter = np.array([x, y])
-
-        self.makeFakeWall()
 
     def clickedPoint(self, msg):
         x = msg.point.x
@@ -83,25 +71,19 @@ class Test_FakeWall(VisualizeLine):
         fakeWall = FakeWall(self.point, self.normal, self.direction, self.rangeMethod)
         linePoints = fakeWall.getLine()
 
-        intersectionPoints = Steer.intersectCirclePoints(linePoints, self.circleCenter, self.radius)
+        sampleState = fakeWall.getSampleCenter()
+        states = [sampleState]
 
-        intersectionLineList = []
-        for intersectionPoint in intersectionPoints:
-            intersectionLineList.append(self.circleCenter)
-            intersectionLineList.append(intersectionPoint)
-
-        numCirclePoints = 100
-        circlePoints = []
-        for i in xrange(numCirclePoints):
-            angle = i*2*np.pi/float(numCirclePoints)
-            point = self.circleCenter + self.radius * np.array([np.cos(angle), np.sin(angle)])
-            circlePoints.append(point)
+        for i in xrange(20):
+            states.append(Sampling.getGaussianState(
+                sampleState, 
+                0.5, 
+                0.5))
 
         self.visualize(linePoints, (1., 0., 0.), publisherIndex=0)
-        self.visualize(circlePoints, (0., 0., 1.), publisherIndex=1)
-        self.visualize(intersectionLineList, (0., 1., 0.), publisherIndex=2, lineList=True)
+        MapUtils.publishStates(states, self.samplePub)
 
 if __name__=="__main__":
-    rospy.init_node("Test_FakeWall")
-    test_fakewall = Test_FakeWall()
+    rospy.init_node("Test_Sampling")
+    test_sampling = Test_Sampling()
     rospy.spin()