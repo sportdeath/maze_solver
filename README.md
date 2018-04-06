e608e2e24f74d1adc87d39560f1e431e5e86b108
Trevor Henderson <tfh@mit.edu>
2018-04-06 17:55:36 -0400

    Reformatting for maze solving final challenge

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# FinalChallenge
-It's the final challenge.
-
-Note that because of ROS naming conventions, the package name is called final_challenge, not FinalChallenge.
+# Maze Solver
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-        packages = ['FinalChallengePy'],
-        package_dir = {'':'.'}
+        packages = ['maze_solver'],
+        package_dir = {'':'src'}
         )
 
 setup(**setup_args)
