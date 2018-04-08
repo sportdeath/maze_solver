341996fd1e2fd198a78b16feac6c9d232f8aba39
Trevor Henderson <tfh@mit.edu>
2018-04-08 14:54:24 -0400

    Updated to play nice with the simulator

--- cartographer_config/install.sh
+++ cartographer_config/install.sh
@@ -7,9 +7,9 @@ fi
 
 DIR="$(realpath $(dirname "$0"))"
 
-ln -s $DIR/racecar_2d.lua $1/src/cartographer_ros/cartographer_ros/configuration_files/
-ln -s $DIR/racecar_2d.launch $1/src/cartographer_ros/cartographer_ros/launch/
-cp -r $DIR/racecar_description $1/src/
+#ln -s $DIR/racecar_2d.lua $1/src/cartographer_ros/cartographer_ros/configuration_files/
+#ln -s $DIR/racecar_2d.launch $1/src/cartographer_ros/cartographer_ros/launch/
+#ln -s $DIR/racecar_description $1/src/
 cd $1
 catkin_make_isolated --install --use-ninja
 source $1/devel_isolated/setup.bash
--- cartographer_config/racecar_2d.lua
+++ cartographer_config/racecar_2d.lua
@@ -2,18 +2,20 @@ include "map_builder.lua"
 include "trajectory_builder.lua"
 
 options = {
+  use_landmarks = false,
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
-  map_frame = "map",
-  tracking_frame = "base_imu_link",
+  map_frame = "cartographer_map",
+  tracking_frame = "imu",
   published_frame = "base_link",
-  odom_frame = "odom",
+  odom_frame = "base_link",
   provide_odom_frame = false,
+  publish_frame_projected_to_2d = false,
   use_odometry = true,
   use_nav_sat = false,
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 2,
+  num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
@@ -23,6 +25,7 @@ options = {
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
+  landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
